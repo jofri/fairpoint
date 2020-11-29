a89d0e6b5634f40be198083d4ba83c0090e90374
Joanna Kang <jfrences11@gmail.com>
2020-11-29 23:57:24 +0000

    feat: add css root style & loding spinner

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import '../style/style.css';
+// import '../style/style.css';
+import './Navbar.css';
+import Loader from './Loader';
+
+
 
 function Navbar () {
   return (
     <>
-      <p>Navbar rendered!</p>
+      <div className="test">
+        <p>Navbar rendered!</p>
+      </div>
+      <div className="test2">
+        <p>styling test</p>
+      </div>
+      <Loader />
     </>
   );
 }
--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 
-  fetch('/api/get')
-    .then(response => response.json())
-    .then(data => console.log('Template data recived form API:', data));
+fetch('/api/get')
+  .then(response => response.json())
+  .then(data => console.log('Template data recived form API:', data));
 
 
 
