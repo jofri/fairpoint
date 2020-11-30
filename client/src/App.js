06d5a210dcb506faf4baba7538f51be894846723
Johan Friedner <j.friedner@gmail.com>
2020-11-30 12:34:13 +0000

    chore: set up component structure

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,10 @@ import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import './style/style.css';
 
-import Homepage from './components/Homepage';
-import Navbar from './components/Navbar';
-import FourOFour from './components/404';
+import NewsFeed from './components/pages/news-feed/News-feed';
+import NewsStory from './components/pages/news-story/News-story';
+import Navbar from './components/navbar';
+import FourOFour from './components/helpers/404';
 
 
 // Import template API services file
@@ -18,8 +19,11 @@ function App () {
       <Navbar/>
       <div className="content">
         <Switch>
-          <Route exact path='/'> {/* If user visits root, redict to homepage */}
-            <Homepage />
+          <Route exact path='/'> {/* If user visits root, redict to homepage/News-feed */}
+            <NewsFeed />
+          </Route>
+          <Route exact path='/story'>
+            <NewsStory />
           </Route>
           <Route exact path='/404'> {/* Specify 404 route */}
             <FourOFour />
